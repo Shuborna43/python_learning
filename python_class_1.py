d87557c6945e09401098f0e9a38040b07f47da6b
Shuborna43 <shubornakhan321@gmail.com>
2025-02-06 21:13:06 +0600

    variable naming convention and arithmatic operations

---
+++ python_class_1.py
@@ -0,0 +1,20 @@
+school_name = "Capstone School"
+
+print (type(school_name))
+
+#variables naming convention:
+
+number1 = 30.5
+#lower snake case
+number1_int = int (number1)
+print ("converted number1 into integer", number1_int)
+
+#myName (lower camel case)
+#MyName (upper camel case)
+
+base_1 = 4
+expo_1 = 3
+expo_ans = base_1 ** expo_1
+print ("printing the answer of function exponent", expo_ans)
+
+
