c1685bb6c5923268d7960c96320523afebcc1b15
Shuborna43 <shubornakhan321@gmail.com>
2025-02-17 21:36:17 +0600

    indexing and slicing

---
+++ indexing_and_slicing.py
@@ -0,0 +1,60 @@
+#strings: indexing and slicing 
+
+my_string_1 = "roses are red"
+
+# char_1 = my_string_1[7]
+
+# print (char_1)
+
+# my_string_1[7] = "g"
+
+# print (my_string_1)
+
+# slice_1 = my_string_1 [6:9:1]
+
+# print (slice_1)
+
+# slice_1 = my_string_1 [6:6]
+
+# print (slice_1)
+
+my_string_2 = "this is a test"
+
+# char_2 = my_string_2 [-2]
+
+# print (char_2)
+
+char_3 = my_string_2 [0:5]
+print (char_3)
+
+char_4 = my_string_2 [0:7]
+print (char_4)
+
+char_5 = my_string_2 [0:14]
+print (char_5)
+
+char_6 = my_string_2 [10:14]
+print (char_6)
+
+
+char_7 = my_string_2 [10:14]
+
+print (char_7,"!")
+print (f"{char_7}!")
+
+char_8 = my_string_2 [:4]
+print (char_8)
+
+char_9= my_string_2 [:14]
+print (char_9)
+
+
+# “THIS”
+# “THIS”
+# “THIS IS”
+# “THIS IS” 
+# “TEST” 
+# “TEST!”
+# “THIS IS A TEST”
+
+
--- practice_5_Feb'25.py
+++ practice_5_Feb'25.py
@@ -11,55 +11,70 @@
 # [You are not allowed to change the code below]
 
 
-class Student:
-    def __init__(self, name, ID, department):
-        self.name=name 
-        self.ID=ID
-        self.department=department
+# class Student:
 
-    def dailyEffort (self,dailyEffort):
-        self.dailyEffort=dailyEffort
+#     def __init__(self, name, ID, department="CSE"):
+#         self.name=name 
+#         self.ID=ID
+#         self.department=department
+        
 
-    def printDetails (self):
+#     def dailyEffort (self,dailyEffort):
+#         self.dailyEffort=dailyEffort
 
-        print ("Name:", self.name)
-        print ("ID:", self.ID)
-        print ("Department:", self.department)
-        print ("Daily Effort:", self.dailyEffort, "hour(s)")
+#     def printDetails (self):
+
+#         print ("Name:", self.name)
+#         print ("ID:", self.ID)
+#         print ("Department:", self.department)
+#         print ("Daily Effort:", self.dailyEffort, "hour(s)")
 
 
-        if self.dailyEffort<=2:
-            print ("Suggestion: Should give more effort!")
-        elif self.dailyEffort<=4:
-            print ("Suggestion: Keep up the good work!")
-        else:
-            print ("Suggestion: Excellent! Now motivate others.")
+#         if self.dailyEffort<=2:
+#             print ("Suggestion: Should give more effort!")
+#         elif self.dailyEffort<=4:
+#             print ("Suggestion: Keep up the good work!")
+#         else:
+#             print ("Suggestion: Excellent! Now motivate others.")
 
 
 # Write your code here.
 
-harry = Student('Harry Potter', 123, "CSC")
-harry.dailyEffort(3)
-harry.printDetails()  
-print('========================')
-john = Student("John Wick", 456, "BBA")
-john.dailyEffort(2)
-john.printDetails()
-print('========================')
-naruto = Student("Naruto Uzumaki", 777, "Ninja")
-naruto.dailyEffort(6)
-naruto.printDetails()
+# harry = Student('Harry Potter', 123)
+# harry.dailyEffort(3)
+# harry.printDetails()  
+# print('========================')
+# john = Student("John Wick", 456, "BBA")
+# john.dailyEffort(2)
+# john.printDetails()
+# print('========================')
+# naruto = Student("Naruto Uzumaki", 777, "Ninja")
+# naruto.dailyEffort(6)
+# naruto.printDetails()
 
 
 # Task:1
 # Write the Farmer class with the required constructor,  methods to get the following output.
 
 class Farmer:
-    def __init__(self):
-        print (................)
+    def __init__(self, name = None):
+        self.name=name
 
-    def addCrops (self,*Crops):
-        self.Crops=Crops
+    def addCrops (self,*Crops,Crops_number):
+        self.Crops = Crops
+        self.Crops_number =len(self.Crops)
+    def addFishes (self,*Fishes, Fish_number):
+        self.Fishes = Fishes 
+        self.Fish_number = len(self.Fishes)
+        if len(self.Fishes)==0:
+            self.Fish_number="don't have any"
+        else:
+            self.Fish_number=len(self.Fishes)
+
+    
+    def showGoods (self):
+        print (f"You have {self.Crops_number} crops: {self.Crops}. You {self.Fish_number} fish")
+    
         
 
 f1 = Farmer()
@@ -92,19 +107,19 @@ f3.showGoods()
 print("-------------------")
 
 
-# Welcome to your farm! 
-# ------------------- 
-# 3 crop(s) added. 
-# ------------------- 
-# No fish added. 
-# ------------------- 
-# 1 crop(s) added. 
-# ------------------- 
-# You have 4 crop(s): 
-# Rice,Jute,Cinnamon,Mustard 
-# You don't have any fish(s). 
-# ------------------- 
-# Welcome to your farm, Korim Mia!
+# # Welcome to your farm! 
+# # ------------------- 
+# # 3 crop(s) added. 
+# # ------------------- 
+# # No fish added. 
+# # ------------------- 
+# # 1 crop(s) added. 
+# # ------------------- 
+# # You have 4 crop(s): 
+# # Rice,Jute,Cinnamon,Mustard 
+# # You don't have any fish(s). 
+# # ------------------- 
+# # Welcome to your farm, Korim Mia!
 #  ------------------- 
 # 2 fish(s) added. 
 # ------------------- 
