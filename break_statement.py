c1db58217061a2c81792800c505415502b53145a
Shuborna43 <shubornakhan321@gmail.com>
2025-03-15 12:47:30 +0600

    break, continue, enumeration

---
+++ break_statement.py
@@ -0,0 +1,57 @@
+#break: break can be used to terminate any loop permanently. 
+
+# def main():
+#     for element in range(0,11,2):
+#         if element > 4:
+#             break 
+#         print(element) 
+# main()
+
+
+# names = ["d","h","g","i","c","e","f","b"]
+
+# for element in names:
+#     if element == "e":
+#         print("found e")
+#         break
+
+#     else:
+#         print("e not found")
+
+
+#Continue statement: continue is used to immediately stop the rest of the current iteration and ask if it should continue to the next iteration. 
+
+# def main():
+#     for element in range(0,11,2):
+#         if (element == 4 or element == 6):
+#             continue
+#         print(element)
+# main()
+
+#program stopping things:
+# pass: code that does nothing at all in a function
+# break: break can be used to terminate any loop permanently. 
+# Continue statement: continue is used to immediately stop the rest of the current iteration and ask if it should continue to the next iteration.
+# return: ends a function immediate. codes written after 'return' will not execute. 
+# exit: ends the program. it permanently deleted/remove all the program code. don't do this. If exit() is used inside a function, it can terminate the entire program abruptly instead of allowing proper cleanup (e.g., closing files, releasing resources).
+
+#enumeration: get index and value in 'for loop'
+
+cities = ("Dhaka", "Rajshahi", "Kulna", "Barisal")
+# for city in cities:
+#     print(city)
+
+# for element, city in enumerate(cities):
+#     print("element is %d for %s" %(element,city))
+
+
+    
+    
+
+
+
+
+
+
+
+
