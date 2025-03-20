0f851ab733405a4551aedffc6cc648bf2f3a44db
Shuborna43 <shubornakhan321@gmail.com>
2025-03-20 21:10:26 +0600

    set-add, update,remove,clear,del,union

--- oop_practice_2.py
+++ oop_practice_2.py
@@ -1,59 +1,98 @@
 #task#1
-class Student():
-    def __init__(self,name,cgpa = 0,credit = 9,department = "CSE",scholarship = " "):
-        self.name = name
-        self.cgpa = float(cgpa)
-        self.credit = int(credit)
-        self.department = department 
-        self.scholarship = scholarship
+# class Student():
+#     def __init__(self,name,cgpa = 0,credit = 9,department = "CSE",scholarship = " "):
+#         self.name = name
+#         self.cgpa = float(cgpa)
+#         self.credit = int(credit)
+#         self.department = department 
+#         self.scholarship = scholarship
     
-    def checkScholarshipEligibility(self):
+#     def checkScholarshipEligibility(self):
        
-        if self.credit >10:
-            if (self.cgpa >= 3.5 and self.cgpa <3.7): 
-                self.scholarship = "Need-based scholarship" 
-                print(f"{self.name} is eligible for {self.scholarship}")
-            elif (self.cgpa >= 3.7): 
-                self.scholarship = "Merit-based scholarship"
-                print(f"{self.name} is eligible for {self.scholarship}")
-            else:
-                self.scholarship = "not eligible for scholarship"
-                print(f"{self.name} is {self.scholarship}")
+#         if self.credit >10:
+#             if (self.cgpa >= 3.5 and self.cgpa <3.7): 
+#                 self.scholarship = "Need-based scholarship" 
+#                 print(f"{self.name} is eligible for {self.scholarship}")
+#             elif (self.cgpa >= 3.7): 
+#                 self.scholarship = "Merit-based scholarship"
+#                 print(f"{self.name} is eligible for {self.scholarship}")
+#             else:
+#                 self.scholarship = "not eligible for scholarship"
+#                 print(f"{self.name} is {self.scholarship}")
 
-        else:
-            self.scholarship = "No scholarship"
-            print(f"{self.name} is not eligible for scholarship")
+#         else:
+#             self.scholarship = "No scholarship"
+#             print(f"{self.name} is not eligible for scholarship")
 
-    def showDetails(self):
-        print (f"Name: {self.name}")
-        print (f"Department: {self.department}")
-        print (f"CGPA: {self.cgpa}")
-        print (f"Number of Credits: {self.credit}")
-        print (f"Scholarship Status: {self.scholarship}")
+#     def showDetails(self):
+#         print (f"Name: {self.name}")
+#         print (f"Department: {self.department}")
+#         print (f"CGPA: {self.cgpa}")
+#         print (f"Number of Credits: {self.credit}")
+#         print (f"Scholarship Status: {self.scholarship}")
 
-print('--------------------------')
-std1 = Student("Alif", 3.99, 12)
-print('--------------------------')
-std1.checkScholarshipEligibility()
-print('--------------------------')
-std1.showDetails()
-print('--------------------------')
-std2 = Student("Mim", 3.4)
-std3 = Student("Henry", 3.5, 15,"BBA")
-print('--------------------------')
-std2.checkScholarshipEligibility()
-print('--------------------------')
-std3.checkScholarshipEligibility()
-print('--------------------------')
-std2.showDetails()
-print('--------------------------')
-std3.showDetails()
-print('--------------------------')
-std4 = Student("Bob", 4.0, 6, "CSE")
-print('--------------------------')
-std4.checkScholarshipEligibility()
-print('--------------------------')
-std4.showDetails()
+# print('--------------------------')
+# std1 = Student("Alif", 3.99, 12)
+# print('--------------------------')
+# std1.checkScholarshipEligibility()
+# print('--------------------------')
+# std1.showDetails()
+# print('--------------------------')
+# std2 = Student("Mim", 3.4)
+# std3 = Student("Henry", 3.5, 15,"BBA")
+# print('--------------------------')
+# std2.checkScholarshipEligibility()
+# print('--------------------------')
+# std3.checkScholarshipEligibility()
+# print('--------------------------')
+# std2.showDetails()
+# print('--------------------------')
+# std3.showDetails()
+# print('--------------------------')
+# std4 = Student("Bob", 4.0, 6, "CSE")
+# print('--------------------------')
+# std4.checkScholarshipEligibility()
+# print('--------------------------')
+# std4.showDetails()
+
+# #task#2
+
+class Foodie():
+    def __init__(self,name):
+        self.name = name
+        self.item_num=[]
+
+    def show_orders(self,*Items):
+        self.Items = Items
+        self.item_num.extend(Items)
+        if len(self.item_num) == 0:
+            print (f"{self.name} has {len(self.item_num)} items in the cart")
+            print (f"Items:[]")
+            print (f"Total spent: 0")
+
+    def order(self,*item-unit):
+        self.item-unit = item-unit
+
+
+
+        
+        
+
+
+menu = {'Chicken Lollipop':15,'Beef Nugget':20,'Americano':180,'Red Velvet':150,'Prawn Tempura':80,'Saute Veg':200}
+
+f1 = Foodie('Frodo')
+print(f1.show_orders())
+print('1----------------------')
+f1.order('Chicken Lollipop-3','Beef Nugget-6','Americano-1')
+print('2----------------------')
+print(f1.show_orders())
+print('3----------------------')
+f1.order('Red Velvet-1')
+print('4----------------------')
+f1.pay_tips(20)
+print('5----------------------')
+print(f1.show_orders())
 
 
 
--- set.py
+++ set.py
@@ -10,9 +10,49 @@
 # my_set = set("rrhiaanfhkllkdr")
 # print(my_set)
 
-# my_set_3 = set(["cat","dog","frog","bird","bird"])
-# print(my_set_3)
+# # my_set_3 = set(["cat","dog","frog","bird","bird"])
+# # print(my_set_3)
 
+# # items can not be accessed in set by refreing to an index.  
+# my_set_4 = {"apple", "banana", "Orange"}
+# for element in my_set_4:
+#     print(element)
+
+# if "apple" in my_set_4:
+#     print("apple was found")
+
+# #add() and update()
+# #once a set is created, its items can not be changed but entire item can be added or removed.
+# my_set_4.add("guava")
+# print(my_set_4)
+
+# my_set_4.update({"multa", "jackfruit", "grape"})
+# print(my_set_4)
+# print(len(my_set_4))
+# #remove an item with remove()-> if the item doesn't exist, remove function will raise an error
+
+# # my_set_4.remove("berry")
+# # print(my_set_4)
+# my_set_4.remove("multa")
+# print(my_set_4)
+
+# #remove an item with discard(). if the item exists it will be removed. but if it does not exist, there will be no error message. 
+# my_set_4.discard("berry")
+# print(my_set_4)
+
+# #clear()-emty set 
+# my_set_5 = {"apple", "banana", "Orange"}
+# my_set_5.clear()
+# print (my_set_5)
+
+# #the "del" keyword will delete the set permanently 
+# del my_set_5
+# print(my_set_5)
+
+#union() returns a new set that contains all items from the 2 sets being unioned 
 my_set_4 = {"apple", "banana", "Orange"}
-for element in my_set_4:
-    print(element)
+my_set_6 = {1,2,3,4}
+new_set = my_set_4.union(my_set_6)
+print(new_set)
+#note: this can also be done with the update()
+
