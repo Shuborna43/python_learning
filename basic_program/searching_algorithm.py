216ad573be05fba5ee7f047efbf646069e96e664
Shuborna43 <shubornakhan321@gmail.com>
2026-05-11 21:11:57 +0600

    bubble sort algorithm

--- basic_program/searching_algorithm.py
+++ basic_program/searching_algorithm.py
@@ -3,29 +3,84 @@
 # characteristics: it works in un-ordered list. it works when there is small number of data 
 
 # binary search algorithm
+#complexity = [0(log n)=order of log n]
 # linear search algorithm 
 
 
 # linear search algorithm
-surovi = [3,5,6,8,9,2]
-find_1 = 8
+# surovi = [3,5,6,8,9,2]
+# find_1 = 8
 
-def search(surovi,find_1):
-    len_sur = len(surovi)
-    #iterate over the array in order to find the key/value x
-    for i in range(0,len_sur): 
-        if (surovi[i] == find_1):
-            return i
-    return -1
+# def search(surovi,find_1):
+#     len_sur = len(surovi)
+#     #iterate over the array in order to find the key/value x
+#     for i in range(0,len_sur): 
+#         if (surovi[i] == find_1):
+#             return i
+#     return -1
 
-result = search(surovi,find_1)
+# result = search(surovi,find_1)
 
-if (result == -1): 
-  print("the value is not in the array")
-else:
-  print(f"The element is in {result} no index")
+# if (result == -1): 
+#   print("the value is not in the array")
+# else:
+#   print(f"The element is in {result} no index")
    
 
+# binary search algorithm
+# using loop
+
+# def binary_search(arr,find_value):
+#    low = 0
+#    high = len(arr)-1
+#    while low <= high:
+#       mid = (low+high)//2
+# # check if find_value is present at mid 
+#       if arr[mid] == find_value:
+#          return mid
+# # if find_value is greater, ignor left half 
+#       elif arr[mid] < find_value:
+#          low = mid+1 
+# # if find_value is smaller, ignor right half
+#       else:
+#          high = mid-1 
+#    return -1 #if we reach here then the element wasn't present 
+
+# arr = [2,3,4,6,7,9]
+# find_value = 10
+
+# result = binary_search(arr,find_value)
+
+# if result != -1:
+#    print("element is present at index", result)
+# else:
+#    print("element is not present at index")
+
+#recursion 
+
+def binary_serach(arr,low,high,find_value): 
+    if high >= low:
+        mid = (low+high)//2
+        if arr[mid] == find_value:
+            return mid
+        elif arr[mid] > find_value:
+            return binary_serach(arr,low,mid-1,find_value)
+        else:
+            return binary_serach(arr,mid+1,high,find_value)
+    else:
+        return -1 #when the element is absent 
+
+arr = [4,5,7,9,11,15]
+find_value = 19
+
+result = binary_serach(arr,0,len(arr)-1,find_value)
+
+if result != -1:
+   print("element is present at index", result)
+else:
+   print("element is not present at index")
+            
+   
    
 
 
---
+++ basic_program/sorting_algorithm.py
@@ -0,0 +1,46 @@
+# in-place sorting: doesn't need extra memory. (e.g. bubble sort and insertion sort)
+# internal vs external sorting: 
+# merge sorting
+# stable sorting
+# hybrid sorting 
+# comparison based sorting-bubblr sort, insertion sort, selection sort, quick sort, merge sort, heap sort
+# non-conparison sorting-counting sort, radix sort  
+
+
+
+# BUBBLE Sort:
+# step 1: adjacent comparison 
+# step 2: swapping 
+# step 3: repetation/ loop 
+
+def bubble_sort(arr):
+    n = len(arr)
+    #traverse/iterates through all array elements 
+    for i in range(n):
+        swapped = False
+        #last i elements are already in place 
+        for j in range(0,n-i-1):
+            # traverse the array from 0 to n-i-1
+            # swap, if the element found is greater than the next element  
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True 
+
+        if swapped == False:
+            break 
+
+# main function
+if __name__ == "__main__":
+    arr = [8,6,2,9,1,0,4]
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
+    
+    bubble_sort(arr)
+    print("\n")
+    print("Sorted Array: ")
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
+    print()
+
+
+
